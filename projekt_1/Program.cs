1e16d68206b0cca198d171f226aa30d6039dd9aa
Nixonseven7 <Nstrandborg@gmail.com>
2026-03-03 11:15:07 +0100

    the Beginning

--- projekt_1/Program.cs
+++ projekt_1/Program.cs
@@ -1,1 +1,28 @@
 ﻿using Raylib_cs;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        //start 
+        Raylib.InitWindow(800, 450, "Dazzling Dreamers");
+        Raylib.SetTargetFPS(60);
+
+
+        while (!Raylib.WindowShouldClose())
+        {
+            //Input
+            if (Raylib.IsKeyPressed(KeyboardKey.Z)) ;
+            if (Raylib.IsKeyPressed(KeyboardKey.X)) ;
+            if (Raylib.IsKeyPressed(KeyboardKey.Period)) ;
+            if (Raylib.IsKeyPressed(KeyboardKey.Minus)) ;
+
+            //Drawing 
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Brown);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
